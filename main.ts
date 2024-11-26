074b0c37b390413916b960c01668404b24f30a6b
AYAN1487 <bujul2023@gmail.com>
2024-11-26 12:11:52 +0530

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+let num = 0
+num += 129
+if (num > 0) {
+    game.splash("Positive Value:", num)
+} else if (num < 0) {
+    game.splash("Negative Value:", num)
+} else {
+    game.splash("Zero Value:", num)
+}
